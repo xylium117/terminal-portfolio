c31befc5648dce2e9707575550cff59f79bf62fc
Ayushman <68391648+AyushmanSarkar@users.noreply.github.com>
2023-09-25 10:18:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/terminal-portfolio/",
+  base: "/",
   plugins: [
     react(),
     VitePWA({
